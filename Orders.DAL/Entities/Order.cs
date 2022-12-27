68435518031c98edddb974e90342f8eaebd794ed
Иван Сафронов <safroniv@mail.ru>
2022-12-27 22:43:17 +0300

    JobLessonASPNETMVCCore06v02. Branch 2.

---
+++ Orders.DAL/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.DAL.Entities
+{
+    public class Order : Entity
+    {
+        public DateTime? OrderDate { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public Buyer Buyer { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+    }
+}
